b5ca11bda98b62d3500bc97e45589121d009fe69
Ubuntu <ubuntu@ip-172-31-13-44.us-west-2.compute.internal>
2023-07-15 21:55:27 +0000

    Upate with token change, evaluation

---
+++ llava/eval/eval_script.sh
@@ -0,0 +1,7 @@
+python model_vqa.py \
+    --model-name alexshengzhili/llava-7b-v0 \
+    --question-file /home/ubuntu/LLaVA-Graph/evaluation/llava_checkpoint/llava_eval_questions.jsonl \
+    --image-folder \
+    /imgs/train \
+    --answers-file \
+    /home/ubuntu/LLaVA-Graph/evaluation/llava_checkpoint/answers.jsonl
